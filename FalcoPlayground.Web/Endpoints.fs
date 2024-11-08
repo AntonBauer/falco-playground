f5492f34023ee27b25fef4995b5d349b4d3026b3
Anton Bauer <anton.bauer2@generic.de>
2024-11-08 14:19:23 +0100

    add port forwarding for postgres container

--- FalcoPlayground.Web/Endpoints.fs
+++ FalcoPlayground.Web/Endpoints.fs
@@ -5,5 +5,5 @@ module Endppoints =
     open Falco
     open Falco.Routing
 
-    module ToDo = 
-        let crud = [all "/todos" [ GET, ToDoHandlers.getAll; POST, ToDoHandlers.create ]]
+    module ToDo =
+        let crud = [ all "/todos" [ GET, ToDoHandlers.getAll; POST, ToDoHandlers.create ] ]
